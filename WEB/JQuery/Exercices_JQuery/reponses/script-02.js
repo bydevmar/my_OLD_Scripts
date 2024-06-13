81260c9a2934cd7687e66ea4454af4fd804142f8
BOUHLALI Abdelfattah <83462650+bydevmar@users.noreply.github.com>
2024-06-13 17:19:31 +0100

    JQuery Exercices

---
+++ WEB/JQuery/Exercices_JQuery/reponses/script-02.js
@@ -0,0 +1,21 @@
+//  chemin relatif : reponses/script-02.js
+// 
+$(function()
+{
+	alert(55);
+	//question 1
+	$(".box").each(function(index){
+		alert(55);
+		$(this).hide();
+	});
+	
+});
+
+
+
+
+
+
+
+
+
